c6cedb91bd60da6ca001d7a3172d5178da7cf67e
Bea David <info@justonestep.de>
2018-01-12 14:59:27 +0100

    update brew.sh

--- install/brew.sh
+++ install/brew.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
-brew tap homebrew/dupes
-brew tap josegonzalez/homebrew-php
-brew tap homebrew/services
+brew tap caskroom/versions
+brew tap buo/cask-upgrade
 
 # install brew packages
-brew install curl
 brew install ghostscript
 brew install git-flow
-brew install httpd
 brew install imagemagick
 brew install macvim
 brew install markdown
@@ -16,42 +13,43 @@ brew install mcrypt
 brew install mysql
 brew install node
 brew install reattach-to-user-namespace
-brew install php55
-brew install php55-mcrypt
-brew install php55-xdebug
 brew install composer
 brew install rbenv
 brew install ruby-build
 brew install ssh-copy-id
-brew install subversion
 brew install the_silver_searcher
 brew install unrar
 brew install wget
 brew install youtube-dl
 
-# brew cask and install software
 
-brew cask install adium
+# brew cask and install software
 brew cask install alfred
-brew cask install bartender
 brew cask install bill
-brew cask install dropbox
+brew cask install carbon-copy-cloner
+brew cask install dash
+brew cask install doxie
 brew cask install firefox
+brew cask install flux
 brew cask install google-chrome
 brew cask install imagealpha
 brew cask install imageoptim
-# brew cask install keyremap4macbook
-brew cask install libreoffice
-brew cask install miro-video-converter
-brew cask install opera
-# brew cask install pckeyboardhack
+brew cask install kap
+brew cask install karabiner-elements
+brew cask install ngrok
+brew cask install ocenaudio
+brew cask install qlcolorcode
+brew cask install qlstephen
 brew cask install sequel-pro
+brew cask install signal
+brew cask install sketch
 brew cask install skype
-brew cask install slate
-brew cask install truecrypt
-brew cask install vagrant
+brew cask install slack
+brew cask install spotify
+brew cask install telegram
 brew cask install virtualbox
 brew cask install vlc
+brew cask install xld
 
 # quicklook stuff
 brew cask install qlcolorcode
