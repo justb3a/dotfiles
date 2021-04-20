e94ac035b328e296a3b9964742f7f40ae579e79f
Tabea David <info@justb3a.com>
2021-04-20 20:15:09 +0200

    Update import js

--- importjs.js
+++ importjs.js
@@ -1,7 +1,16 @@
 // https://github.com/Galooshi/import-js
+// Available environments: https://github.com/sindresorhus/globals/blob/main/globals.json
+// builtin, es5, es2015, es2017, es2020, es2021, browser, worker, nodeBuildin, commonjs, jasmine, jest, qnit, phantomjs, couch, rhino, nashorn, wsh, jquery, yui, shelljs, prototypejs, meteor, mongo, applescript, serviceworker, atomtest, emertest, protractor, shared-node-browser, webextensions, greasemonkey, devtools
 module.exports = {
   excludes: ['./dist/**'],
+  environments: ['node', 'jest', 'es2017'],
+  declarationKeyword: 'import',
   emptyLineBetweenGroups: false,
   groupImports: false,
   sortImports: false,
+  // importDevDependencies: true,
+  aliases: {
+    styled: 'styled-components',
+    React: 'react',
+  },
 };
