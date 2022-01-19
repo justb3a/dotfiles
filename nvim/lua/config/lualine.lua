f03545d47ede957d84e23970e92956cc07efbca2
justb3a <info@justb3a.com>
2022-01-19 09:47:34 +0100

    Vim: Define lua line sections

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -2,4 +2,12 @@ require('lualine').setup({
   options = {
     theme = 'gruvbox',
   },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
 })
