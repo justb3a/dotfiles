d5dd326db19b9ba848e58e7b66828c7f61dc4a6a
justb3a <info@justb3a.com>
2022-02-09 11:24:45 +0100

    Nvim: cmp config disable tmux all_panes
    Because it hurts the performance

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -44,7 +44,7 @@ cmp.setup({
     { name = 'nvim_lsp' },
     { name = 'path' },
     { name = 'vsnip' },
-    { name = 'tmux', option = { all_panes = true } }, -- TODO 'true' makes things sloppy
+    { name = 'tmux', option = { all_panes = false } },
     { name = 'calc' },
   }),
   formatting = {
