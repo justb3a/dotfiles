29880cbd5a1cd3e183ded52a099239c54d2366a7
justb3a <info@justb3a.com>
2022-07-28 13:15:41 +0200

    Add woffify script

--- install/4_webdev.sh
+++ install/4_webdev.sh
@@ -3,10 +3,11 @@
 brew install imagemagick
 brew install jq
 
-  `jq .scripts package.json`
+`jq .scripts package.json`
 
 # additional webdev cli tools
 npm install -g depcheck
 npm install -g npm-check
 npm install -g npm-check-updates
 npm install -g yarn
+npm install -g ttf2woff
