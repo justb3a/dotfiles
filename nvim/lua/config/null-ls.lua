0523022a0dbfde9d2554231539a54ddd848ce008
justb3a <info@justb3a.com>
2023-01-03 13:44:11 +0100

    Update null ls config

--- nvim/lua/config/null-ls.lua
+++ nvim/lua/config/null-ls.lua
@@ -32,7 +32,7 @@ local sources = {
 null_ls.setup({
   sources = sources,
   on_attach = function(client)
-    if client.resolved_capabilities.document_formatting then
+    if client.server_capabilities.document_formatting then
       vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()")
     end
   end,
