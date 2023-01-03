d367d68711d1ec46329232678e7ecbf58cc00e1c
justb3a <info@justb3a.com>
2023-01-03 15:28:28 +0100

    Update quicklook settings

--- install/5_software.sh
+++ install/5_software.sh
@@ -6,17 +6,18 @@ brew tap homebrew/cask-versions
 brew install --cask firefox
 brew install --cask google-chrome
 brew install --cask iterm2
-brew install --cask qlstephen
-brew install --cask syntax-highlight
 brew install --cask notion
 brew install --cask telegram
 brew install --cask visual-studio-code
 brew install --cask zoom
 
-brew install --cask rectangle
 # afterwards, import settings from : `RectangeConfig.json`
+brew install --cask rectangle
 
-# Quicklook-reset
+# Quick Look syntax highlight
+brew install --cask syntax-highlight
+
+# Quicklook-reset, kill finder, start App "Syntax Highlight"
 qlmanage -r
 qlmanage -r cache
 
