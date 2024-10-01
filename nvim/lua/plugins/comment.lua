d00f6635d65b1b31b2580d9689e450a91950f82d
justb3a <info@justb3a.com>
2024-10-01 14:22:46 +0200

    Update comment key mappings

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -11,6 +11,13 @@ return {
         return vim.bo.commentstring
       end,
     }
+
+    vim.keymap.set('n', '<leader>.', '<cmd>lua require"Comment.api".toggle.linewise.current()<cr>')
+    vim.keymap.set('v', '<leader>.', '<esc><cmd>lua require"Comment.api".toggle.linewise(vim.fn.visualmode())<cr>')
+    -- map('n', '<leader>.b', '<cmd>lua require"Comment.api".toggle_current_blockwise()<cr>')
+    -- map('v', '<leader>.b', '<esc><cmd>lua require"Comment.api".toggle_blockwise_op(vim.fn.visualmode())<cr>')
+    -- map('n', '<leader>.o', '<cmd>lua require"Comment.api".insert_linewise_above()<cr>')
+    -- map('n', '<leader>.a', '<cmd>lua require"Comment.api".insert_linewise_eol()<cr>')
   end,
   lazy = false,
   dependencies = {
