feadf685cc4f20cc453ae46d559f850f25d74767
justb3a <info@justb3a.com>
2024-10-07 14:29:43 +0200

    Add tsc.nvim

--- nvim/lua/plugins/typescript.lua
+++ nvim/lua/plugins/typescript.lua
@@ -37,4 +37,31 @@ return {
       },
     },
   },
+  {
+    'dmmulroy/tsc.nvim',
+    config = function()
+      require('tsc').setup {
+        auto_open_qflist = false,
+        auto_close_qflist = false,
+        auto_focus_qflist = false,
+        auto_start_watch_mode = true,
+        use_trouble_qflist = false,
+        use_diagnostics = false,
+        run_as_monorepo = false,
+        -- bin_path = utils.find_tsc_bin(),
+        enable_progress_notifications = false,
+        enable_error_notifications = false,
+        flags = {
+          --   noEmit = true,
+          --   project = function()
+          --     return utils.find_nearest_tsconfig()
+          --   end,
+          watch = true,
+        },
+        hide_progress_notifications_from_history = true,
+        spinner = { '⣾', '⣽', '⣻', '⢿', '⡿', '⣟', '⣯', '⣷' },
+        pretty_errors = true,
+      }
+    end,
+  },
 }
