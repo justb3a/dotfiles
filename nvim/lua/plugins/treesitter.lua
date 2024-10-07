8965eca56d39f444cd1f2159a1ec315a352dfc2e
justb3a <info@justb3a.com>
2024-10-07 08:21:49 +0200

    Fix nvim-ts-autotag setup to not break syntax highlighting

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,55 +1,65 @@
-return { -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  -- event = { "BufReadPre", "BufNewFile" },
-  event = 'VeryLazy',
-  dependencies = { 'windwp/nvim-ts-autotag' },
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-  config = function()
-    require('nvim-ts-autotag').setup()
-  end,
-  opts = {
-    ensure_installed = {
-      'bash',
-      'c',
-      'comment',
-      'css',
-      'diff',
-      'html',
-      'javascript',
-      'jsdoc',
-      'json',
-      'json5',
-      'lua',
-      'luadoc',
-      'markdown',
-      'markdown_inline',
-      'query',
-      'regex',
-      'scss',
-      'tsx',
-      'typescript',
-      'yaml',
-      'vim',
-      'vimdoc',
-    },
-    -- Autoinstall languages that are not installed
-    auto_install = true,
-    highlight = { enable = true },
-    indent = { enable = true },
-    autotag = { enable = true },
-    context_commentstring = {
-      config = {
-        javascript = {
-          __default = '// %s',
-          jsx_element = '{/* %s */}',
-          jsx_fragment = '{/* %s */}',
-          jsx_attribute = '// %s',
-          comment = '// %s',
+-- Highlight, edit, and navigate code
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    opts = {
+      ensure_installed = {
+        'bash',
+        'c',
+        'comment',
+        'css',
+        'diff',
+        'html',
+        'javascript',
+        'jsdoc',
+        'json',
+        'json5',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'regex',
+        'scss',
+        'tsx',
+        'typescript',
+        'yaml',
+        'vim',
+        'vimdoc',
+      },
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      autotag = { enable = true },
+      context_commentstring = {
+        config = {
+          javascript = {
+            __default = '// %s',
+            jsx_element = '{/* %s */}',
+            jsx_fragment = '{/* %s */}',
+            jsx_attribute = '// %s',
+            comment = '// %s',
+          },
+          typescript = { __default = '// %s', __multiline = '/* %s */' },
         },
-        typescript = { __default = '// %s', __multiline = '/* %s */' },
       },
     },
+  },
+  {
+    'windwp/nvim-ts-autotag',
+    event = 'BufReadPre',
+    config = function()
+      require('nvim-ts-autotag').setup {
+        opts = {
+          enable_close = true, -- Auto close tags
+          enable_rename = true, -- Auto rename pairs of tags
+          enable_close_on_slash = true, -- Auto close on trailing </
+        },
+      }
+    end,
   },
 }
