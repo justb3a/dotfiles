0163d218170ef7fec9fb7593c9ab84dd528a298a
justb3a <info@justb3a.com>
2024-10-07 10:40:00 +0200

    Even treesitter is working

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,42 +1,58 @@
--- Treesitter for nicer syntax-highlighting
+-- Highlight, edit, and navigate code
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  dependencies = {
-    'windwp/nvim-ts-autotag',
-  },
-  config = function()
-    require('nvim-treesitter.configs').setup {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    opts = {
       ensure_installed = {
         'bash',
+        'c',
         'comment',
         'css',
+        'diff',
         'html',
         'javascript',
         'jsdoc',
         'json',
         'json5',
         'lua',
-        'php',
-        'python',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
         'regex',
-        'ruby',
         'scss',
-        'toml',
         'tsx',
         'typescript',
-        'vue',
         'yaml',
+        'vim',
+        'vimdoc',
       },
-      highlight = {
-        enable = true,
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+      autotag = { enable = false },
+      context_commentstring = {
+        config = {
+          javascript = {
+            __default = '// %s',
+            jsx_element = '{/* %s */}',
+            jsx_fragment = '{/* %s */}',
+            jsx_attribute = '// %s',
+            comment = '// %s',
+          },
+          typescript = { __default = '// %s', __multiline = '/* %s */' },
+        },
       },
-      indent = {
-        enable = true,
-      },
-      autotag = {
-        enable = true,
-      },
-    }
-  end,
+    },
+  },
+  {
+    'windwp/nvim-ts-autotag',
+    config = function()
+      require('nvim-ts-autotag').setup()
+    end,
+  },
 }
