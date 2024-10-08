f82a5a787b167d8e19f2cd6a933217910864c715
justb3a <info@justb3a.com>
2024-10-08 15:00:22 +0200

    Fancy command line

---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,10 @@
+return {
+  'folke/noice.nvim', -- fancy command line/search bar
+  event = 'VeryLazy',
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+  },
+}
