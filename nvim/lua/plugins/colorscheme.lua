bb63f520918f0bf33f416cd5acab13eba0e33d4e
justb3a <info@justb3a.com>
2024-10-09 08:25:18 +0200

    Add plugin colorizer

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,28 @@
 -- Consider: https://github.com/catppuccin/nvim
 return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  priority = 1000,
-  config = function()
-    require('catppuccin').setup()
-    vim.cmd.colorscheme 'catppuccin'
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup()
+      vim.cmd.colorscheme 'catppuccin'
 
-    -- You can configure highlights by doing something like:
-    vim.cmd.hi 'Comment gui=none'
-  end,
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+  {
+    'norcalli/nvim-colorizer.lua',
+    event = 'BufReadPre',
+    config = function()
+      require('colorizer').setup({
+        'css',
+        'scss',
+        'json',
+        'json5',
+        'javascript',
+      }, { css = true })
+    end,
+  },
 }
