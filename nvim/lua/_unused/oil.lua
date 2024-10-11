424c32081f453d4fa6e6ec5fb9c054ebabd98156
justb3a <info@justb3a.com>
2024-10-11 12:18:54 +0200

    Try plugin oil

---
+++ nvim/lua/_unused/oil.lua
@@ -0,0 +1,9 @@
+-- A file explorer that lets you edit your filesystem like a normal Neovim buffer.
+return {
+  'stevearc/oil.nvim',
+  config = function()
+    local oil = require 'oil'
+    oil.setup()
+    vim.keymap.set('n', '-', oil.toggle_float, {})
+  end,
+}
