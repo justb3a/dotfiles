a0d966f718779fe9cf8cc93e1d21c9abb0e3b688
justb3a <info@justb3a.com>
2024-10-22 13:05:40 +0200

    Extend nvim test config

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -3,15 +3,36 @@
 
 return {
   'vim-test/vim-test',
+  dependencies = { 'preservim/vimux' },
   config = function()
-    vim.keymap.set('n', '<leader>t', ':TestNearest<CR>', {})
-    vim.keymap.set('n', '<leader>T', ':TestFile<CR>', {})
+    vim.keymap.set('n', '<leader>t', ':TestFile<CR>', {})
+    vim.keymap.set('n', '<leader>T', ':TestNearest<CR>', {})
     vim.keymap.set('n', '<leader>a', ':TestSuite<CR>', {})
     vim.keymap.set('n', '<leader>l', ':TestLast<CR>', {})
     vim.keymap.set('n', '<leader>g', ':TestVisit<CR>', {})
 
-    vim.g['test#strategy'] = 'neovim'
     vim.g['test#javascript#runner'] = 'vitest'
     vim.g['test#typescript#runner'] = 'vitest'
+
+    -- vim.g['test#strategy'] = 'vimux'
+    -- vim.g['test#strategy'] = 'neovim'
+    vim.g['test#strategy'] = 'neovim_sticky'
+
+    -- Reopen split if not visible
+    vim.g['test#neovim_sticky#reopen_window'] = 1
+
+    -- Try to abort previous run
+    vim.g['test#neovim_sticky#kill_previous'] = 1
+
+    -- Have the terminal open in normal mode, so it does not close on key press
+    vim.g['test#neovim_sticky#start_normal'] = 1
+
+    -- clear screen from previous run
+    vim.g['test#preserve_screen'] = 0
+    -- vim.cmd 'let g:test#preserve_screen = 0'
+
+    -- do not output test command
+    vim.g['test#echo_command'] = 0
+    -- vim.cmd 'let g:test#echo_command = 0'
   end,
 }
