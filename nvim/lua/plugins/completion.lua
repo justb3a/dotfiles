a0432e506b9ab5798cf868b3d0584ece69382d43
justb3a <info@justb3a.com>
2025-05-08 16:04:15 +0200

    Disable completion luasnip lazydev calc

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -2,34 +2,34 @@ return { -- Autocompletion
   'hrsh7th/nvim-cmp',
   event = 'InsertEnter',
   dependencies = {
-    {
-      'L3MON4D3/LuaSnip',
-      dependencies = {
-        'rafamadriz/friendly-snippets',
-        config = function()
-          require('luasnip.loaders.from_vscode').lazy_load()
-        end,
-      },
-    },
-    'saadparwaiz1/cmp_luasnip',
-    'andersevenrud/cmp-tmux',
+    -- {
+    --   'L3MON4D3/LuaSnip',
+    --   dependencies = {
+    --     'rafamadriz/friendly-snippets',
+    --     config = function()
+    --       require('luasnip.loaders.from_vscode').lazy_load()
+    --     end,
+    --   },
+    -- },
+    -- 'saadparwaiz1/cmp_luasnip',
+    -- 'andersevenrud/cmp-tmux',
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-buffer',
-    'hrsh7th/cmp-cmdline',
-    'hrsh7th/cmp-calc',
+    -- 'hrsh7th/cmp-cmdline',
+    -- 'hrsh7th/cmp-calc',
   },
   config = function()
     local cmp = require 'cmp'
-    local luasnip = require 'luasnip'
-    luasnip.config.setup {}
+    -- local luasnip = require 'luasnip'
+    -- luasnip.config.setup {}
 
     cmp.setup {
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
+      -- snippet = {
+      --   expand = function(args)
+      --     luasnip.lsp_expand(args.body)
+      --   end,
+      -- },
       completion = { completeopt = 'menu,menuone,noinsert' },
 
       -- See `:help ins-completion` or https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
@@ -43,24 +43,24 @@ return { -- Autocompletion
 
         -- <c-l> will move you to the right of each of the expansion locations.
         -- <c-h> is similar, except moving you backwards.
-        ['<C-l>'] = cmp.mapping(function()
-          if luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          end
-        end, { 'i', 's' }),
-        ['<C-h>'] = cmp.mapping(function()
-          if luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          end
-        end, { 'i', 's' }),
+        -- ['<C-l>'] = cmp.mapping(function()
+        --   if luasnip.expand_or_locally_jumpable() then
+        --     luasnip.expand_or_jump()
+        --   end
+        -- end, { 'i', 's' }),
+        -- ['<C-h>'] = cmp.mapping(function()
+        --   if luasnip.locally_jumpable(-1) then
+        --     luasnip.jump(-1)
+        --   end
+        -- end, { 'i', 's' }),
       },
       sources = {
         { name = 'nvim_lsp' },
         { name = 'path' },
         { name = 'buffer', keyword_length = 3 },
-        { name = 'luasnip', keyword_length = 2 },
-        { name = 'lazydev', group_index = 0 },
-        { name = 'calc' },
+        -- { name = 'luasnip', keyword_length = 2 },
+        -- { name = 'lazydev', group_index = 0 },
+        -- { name = 'calc' },
       },
     }
   end,
